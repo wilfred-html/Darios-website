df9931b8a648b1908c8bf407a80d14c14cc8eb2c
wilfred <wolfdog.singini@gmail.com>
2023-05-25 16:53:21 +0200

    started main meals and used JS for mulit pages

--- script.js
+++ script.js
@@ -18,9 +18,35 @@ document.addEventListener("DOMContentLoaded", function () {
   function scrollHandler() {
     clearTimeout(fadeTimeout);
     fadeTimeout = setTimeout(checkFadeElements, 5);
-  } 
+  }
 
   window.addEventListener("scroll", scrollHandler);
   checkFadeElements();
 });
 
+// container information
+const breakFast = document.querySelector(".breakfast-container");
+const main = document.querySelector(".main-container");
+const beverage = document.querySelector(".beverage-container");
+const drinks = document.querySelector(".drink-container");
+const desert = document.querySelector(".desert-container");
+
+// conatainer triggers
+const breakFastBtn = document.querySelector(".breakfast-btn");
+const mainBtn = document.querySelector(".main-btn");
+const beverageBtn = document.querySelector(".beverage-btn");
+const drinkBtn = document.querySelector(".drink-btn");
+const desertBtn = document.querySelector(".desert-btn");
+
+breakFast.style.display = "none";
+
+
+mainBtn.addEventListener("click", function () {
+  breakFast.style.display = "none";
+  main.style.display = "block";
+  beverage.style.display = "none";
+  drinks.style.display = "none";
+  desert.style.display = "none";
+
+
+});
