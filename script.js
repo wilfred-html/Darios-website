baf621a9a7c60ccfd677c11aa750e279eeec69c1
wilfred <wolfdog.singini@gmail.com>
2023-05-26 21:14:57 +0200

    Menu: color scheme changed drasticly improved look

--- script.js
+++ script.js
@@ -1,73 +1,36 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   let fadeElements = document.querySelectorAll(".fade-in");
-//   let fadeTimeout;
-
-//   function checkFadeElements() {
-//     for (let i = 0; i < fadeElements.length; i++) {
-//       let fadeElement = fadeElements[i];
-//       let positionFromTop = fadeElement.getBoundingClientRect().top;
-
-//       if (positionFromTop - window.innerHeight <= 0) {
-//         fadeElement.classList.add("show");
-//       } else {
-//         fadeElement.classList.remove("show");
-//       }
-//     }
-//   }
-
-//   function scrollHandler() {
-//     clearTimeout(fadeTimeout);
-//     fadeTimeout = setTimeout(checkFadeElements, 5);
-//   }
-
-//   window.addEventListener("scroll", scrollHandler);
-//   checkFadeElements();
-// });
-
-// container information
-
 const breakFast = document.querySelector(".breakfast-container");
 const main = document.querySelector(".main-container");
 const beverage = document.querySelector(".beverage-container");
 const drinks = document.querySelector(".drink-container");
 const desert = document.querySelector(".desert-container");
 
-// conatainer triggers
+// container triggers
 const breakFastBtn = document.querySelector(".breakfast-btn");
 const mainBtn = document.querySelector(".main-btn");
 const beverageBtn = document.querySelector(".beverage-btn");
 const drinkBtn = document.querySelector(".drink-btn");
 const desertBtn = document.querySelector(".desert-btn");
 
-// Default Visibility 
+// Default Visibility
 breakFast.style.display = "none";
 main.style.display = "block";
-breakFastBtn.style.backgroundColor = "darkblue";
+breakFastBtn.style.backgroundColor = "#3e6448";
+
+// Set starting colors
+mainBtn.style.backgroundColor = "#53835f";
 
 mainBtn.addEventListener("click", function () {
   breakFast.style.display = "none";
   main.style.display = "block";
 
-  // beverage.style.display = "none";
-  // drinks.style.display = "none";
-  // desert.style.display = "none";
-
-  mainBtn.style.backgroundColor = "darkblue";
-
-
-  // colors
-  breakFastBtn.style.backgroundColor = "rgb(3, 121, 194)";
+  mainBtn.style.backgroundColor = "#3e6448";
+  breakFastBtn.style.backgroundColor = "#53835f";
 });
 
 breakFastBtn.addEventListener("click", function () {
-  // Visible Containers
   breakFast.style.display = "block";
   main.style.display = "none";
-  // beverage.style.display = "none";
-  // drinks.style.display = "none";
-  // desert.style.display = "none";
 
-   // colors
-   breakFastBtn.style.backgroundColor = "darkblue";
-   mainBtn.style.backgroundColor = "rgb(3, 121, 194)";
+  breakFastBtn.style.backgroundColor = "#3e6448";
+  mainBtn.style.backgroundColor = "#53835f";
 });
